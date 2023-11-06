f1879256035c0e87a51f9296fe77d1b6f34c2666
SergioSboy <118937788+SergioSboy@users.noreply.github.com>
2023-11-06 18:57:23 +0300

    2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,26 @@
-import React, {useState}  from "react";
+import React, {useRef, useState}  from "react";
 import "./styles/App.css"
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 function App() {
+
   const [posts, setPost] = useState([
     {id: 1, title:"JavaScript", body: 'Description'},
     {id: 2, title:"JavaScript 1", body: 'Description'},
     {id: 3, title:"JavaScript 5", body: 'Description'}
-  ])
 
-  const [title, setTitle] = useState('')
-  const addNewPost = (e) => {
-    e.preventDefault()
+  ])
+  const createPost = (newPost) => {
+    setPost([...posts, newPost])
+  }
+  
+  const removePost = (post) =>{
+    setPost(posts.filter(p => p.id !== post.id))
   }
   return (
     <div className="App">
-      <form>
-        <MyInput
-          value = {title}
-          onChange = {e => setTitle(e.target.value)}
-          type = 'text' 
-          placeholder = 'Name of Post'
-        />
-        <MyInput type = 'text' placeholder = 'Overview'></MyInput>
-        <MyButton onClick = {addNewPost}> Create Post</MyButton>
-      </form>
-      <PostList posts={posts} title = 'Post List about JS'/>
+      <PostForm create = {createPost}/>
+      <PostList remove = {removePost} posts={posts} title = 'Post List about JS'/>
     </div>
   );
 }
