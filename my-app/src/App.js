8ad570bfb21cb1d6b45ea31085b9d182bcfaacaf
SergioSboy <118937788+SergioSboy@users.noreply.github.com>
2023-11-07 20:00:29 +0300

    07.11.2023
    SortedPost, PostFilter, SerchPost (by name, by owerview)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,31 @@
-import React, {useRef, useState}  from "react";
+import React, {useRef, useState, useMemo}  from "react";
 import "./styles/App.css"
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
+import PostFilter from "./components/PostFilter";
 function App() {
 
   const [posts, setPost] = useState([
-    {id: 1, title:"JavaScript", body: 'Description'},
-    {id: 2, title:"JavaScript 1", body: 'Description'},
-    {id: 3, title:"JavaScript 5", body: 'Description'}
+    {id: 1, title:"аа", body: 'бб'},
+    {id: 2, title:"гг 2", body: 'аа'},
+    {id: 3, title:"вв 3", body: 'яя'}
 
   ])
+  const [filter, setFilter] = useState({sort: '', query: ''})
+
+  const sortedPosts = useMemo(() => {
+    if(filter.sort) { 
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+    }
+    return posts;
+  }, [filter.sort, posts])
+
+  const sortedAndSerchedPosts = useMemo(() => {
+    return sortedPosts.filter(post =>  post.title.toLowerCase().includes(filter.query))
+  }, [filter.query, sortedPosts])
+
   const createPost = (newPost) => {
     setPost([...posts, newPost])
   }
@@ -17,10 +33,16 @@ function App() {
   const removePost = (post) =>{
     setPost(posts.filter(p => p.id !== post.id))
   }
+
   return (
     <div className="App">
       <PostForm create = {createPost}/>
-      <PostList remove = {removePost} posts={posts} title = 'Post List about JS'/>
+      <hr style={{margin: '15px 0'}}/>
+      <PostFilter 
+        filter={filter} 
+        setFilter={setFilter}
+      />
+      <PostList remove = {removePost} posts={sortedAndSerchedPosts} title = 'Post List about JS'/>      
     </div>
   );
 }
--- my-app/src/components/PostList.jsx
+++ my-app/src/components/PostList.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import PostItem from "./PostItem";
 const PostList = ({posts, title, remove}) => {
+    if (!posts.length){
+        return <h1 style = {{textAlign: 'center'}}>
+            Not Posts
+        </h1>
+    }
     return (
         <div>
         <h1 style={{textAlign:'center'}}> 
