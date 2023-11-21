f732693aeb8d6eb75875e22b3eb71c7a3d50092f
SergioSboy <118937788+SergioSboy@users.noreply.github.com>
2023-11-21 17:44:01 +0300

    2

---
+++ social-network/src/Techno.js
@@ -0,0 +1,14 @@
+
+const Tehno = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Tehno;
