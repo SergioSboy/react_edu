03cd1c0b66b4f068456186765bfd0ca029067127
SergioSboy <118937788+SergioSboy@users.noreply.github.com>
2023-11-22 19:28:59 +0300

    12

---
+++ social-network/src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+const Profile = () => {
+    return(
+        <div className='content'>
+        <div>
+          <img src='https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.1803636316.1700524800&semt=sph' />
+        </div>
+        <div>
+
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile;
