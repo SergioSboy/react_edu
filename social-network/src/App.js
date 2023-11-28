02e1559033cd3cbbdd69f37edf9fbaaad55c1f0f
SergioSboy <118937788+SergioSboy@users.noreply.github.com>
2023-11-28 21:49:48 +0300

    18
    component Dialogs, css-module Dialogs

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import Navigation from './components/Navigation/Navigation';
+import Dialogs from "./components/Dialogs/Dialogs";
+
 function App() {
-  return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navigation />
-      <Profile />
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navigation/>
+            {/*<Profile />*/}
+            <div className='app-wrapper-content'>
+                <Profile />
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
