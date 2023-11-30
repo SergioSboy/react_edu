d88df5d96421194dab52bd53f3a4d045fb55dc4e
SergioSboy <118937788+SergioSboy@users.noreply.github.com>
2023-11-30 12:57:28 +0300

    20
    NavLink, class Active

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -4,18 +4,22 @@ import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import Navigation from './components/Navigation/Navigation';
 import Dialogs from "./components/Dialogs/Dialogs";
+import {Route, BrowserRouter, Routes} from "react-router-dom";
 
 function App() {
     return (
+        <BrowserRouter>
         <div className='app-wrapper'>
             <Header/>
             <Navigation/>
-            {/*<Profile />*/}
             <div className='app-wrapper-content'>
-                <Profile />
+                <Routes>
+                    <Route path="/profile" element={<Profile/>}/>
+                    <Route path="/dialogs" element={<Dialogs/>}/>
+                </Routes>
             </div>
-
         </div>
+        </BrowserRouter>
     );
 }
 
--- social-network/src/components/Navigation/Navigation.jsx
+++ social-network/src/components/Navigation/Navigation.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import classes from './Navigation.module.css'
+import {NavLink} from "react-router-dom";
+
 const Navigation = () => {
     return (
         <nav className={classes.nav}>
             <div>
                 <img src="https://cdn-icons-png.flaticon.com/512/6915/6915987.png"/>
-                <a href='#'>Profile</a>
+                <NavLink to='/profile' className = { navData => navData.isActive ? classes.active : classes.nav }>Profile</NavLink>
             </div>
             <div>
                 <img src="https://secure.webtoolhub.com/static/resources/icons/set51/a0e45edb.png"/>
-                <a href='#'>Messages</a>
+                <NavLink to='/dialogs'>Messages</NavLink>
             </div>
             <div>
                 <img src="https://cdn-icons-png.flaticon.com/512/3799/3799802.png"/>
-                <a href='#'>News</a>
+                <NavLink to='#'>News</NavLink>
             </div>
             <div>
                 <img src="https://www.svgrepo.com/show/224210/musical-note-music-player.svg"/>
-                <a href='#'>Music</a>
+                <NavLink to='#'>Music</NavLink>
             </div>
             <div>
                 <img src="https://secure.webtoolhub.com/static/resources/icons/set98/808d3f7d.png"/>
                 <a href='#'>Settings</a>
             </div>
-      </nav>
+        </nav>
     )
 }
 
