b8ffccad3bbfd9ad7d57693d75dfe82653806422
SergioSboy <118937788+SergioSboy@users.noreply.github.com>
2023-12-02 18:17:41 +0300

    21
    Content dialogs(messages, items)

--- social-network/src/components/Dialogs/Dialogs.jsx
+++ social-network/src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,30 @@ import React from "react";
 import classes from './Dialogs.module.css'
 const Dialogs = (props) => {
     return (
-        <div className={classes.content}>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogs_items}>
+                <div className={classes.dialog}>
+                    Dmitry
+                </div>
+                <div className={classes.dialog}>
+                    Aleksey
+                </div>
+                <div className={classes.dialog}>
+                    Anton
+                </div>
+                <div className={classes.dialog}>
+                    Lioba
+                </div>
+                <div className={classes.dialog}>
+                    Sveta
+                </div>
+            </div>
+            <div className={classes.create_line}></div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How are you? Welcome my site!</div>
+                <div className={classes.message}>Yo</div>
+            </div>
         </div>
     )
 }
