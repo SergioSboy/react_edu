5642c5c8e79a49e15c35bfae46eb1de6941e27f2
SergioSboy <118937788+SergioSboy@users.noreply.github.com>
2024-02-29 20:05:20 +0300

    75
    Redux-form

--- social-network/src/components/Login/Login.jsx
+++ social-network/src/components/Login/Login.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import MyButton from "../MyButton/MyButton";
+import {Field, reduxForm} from "redux-form";
 
-const Login = () => {
+
+const Login = (props) => {
     return (
         <div>
-            <h1>LoginPage</h1>
+            <h1>Login</h1>
+            <LoginReduxForm/>
         </div>
     );
 };
 
+const LoginForm = (props) => {
+    return (
+        <div>
+            <form>
+                <div>
+                    <Field placeholder={'Login'} component={'input'} name={'login'}/>
+                </div>
+                <div>
+                    <Field placeholder={'Password'} component={'input'} name={'password'}/>
+                </div>
+                <div>
+                    <Field type={'checkbox'} component={'input'} name={'rememberMe'}/> remember me
+                </div>
+                <div>
+                    <MyButton name={'Login'}/>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+const LoginReduxForm = reduxForm({form:'login'})(LoginForm)
 export default Login;
