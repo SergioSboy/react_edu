0ad3c4a9167007524f7cd8f9ecce5e4317c6bb55
SergioSboy <118937788+SergioSboy@users.noreply.github.com>
2024-03-11 16:46:40 +0300

    85
    hook useEffect

--- social-network/src/components/Profile/ProfileInfo/ProfileStatusWithHooks.js
+++ social-network/src/components/Profile/ProfileInfo/ProfileStatusWithHooks.js
@@ -1,21 +1,25 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import MyInput from "../../MyInput/MyInput";
 
 const ProfileStatusWithHooks = (props) => {
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status]);
     const activateMode = () => {
         setEditMode(true)
-    }
+    };
 
     const deactivateEditMode = () => {
         setEditMode(false);
         props.updateStatus(status)
-    }
+    };
 
     const onStatusChange = (e) => {
         setStatus(e.target.value)
-    }
+    };
     return (
         <div>
             {!editMode &&
