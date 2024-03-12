6e7bca9a32b9ab68a4b8e9974b366f5611b9e70e
SergioSboy <118937788+SergioSboy@users.noreply.github.com>
2024-03-12 19:36:29 +0300

    88

--- social-network/src/components/Profile/MyPosts/MyPosts.jsx
+++ social-network/src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,11 +5,13 @@ import MyButton from "../../MyButton/MyButton";
 import {Field, reduxForm} from "redux-form";
 import {maxLengthCreator, required} from "../../../utils/validators/validators";
 import {Textarea} from "../../common/FormsControls/FormControls";
+
 const AddNewPostForm = (props) => {
     return (
         <form className={classes.play} onSubmit={props.handleSubmit}>
             <div className={classes.my_inp}>
-                <Field component={Textarea} placeholder='Create...' name='newPostText' validate={[required, maxLengthCreator(10)]}/>
+                <Field component={Textarea} placeholder='Create...' name='newPostText'
+                       validate={[required, maxLengthCreator(10)]}/>
             </div>
 
             <div className={classes.my_btn}>
@@ -21,7 +23,7 @@ const AddNewPostForm = (props) => {
 
 const AddNewPostFormRedux = reduxForm({form: 'newPostForm'})(AddNewPostForm);
 
-const MyPosts = (props) => {
+const MyPosts = React.memo(props => {
 
     let postsElements = props.profilePage.postsData.map(el => <Post message={el.message} key={el.id} id={el.id}
                                                                     img={el.img}/>)
@@ -45,7 +47,7 @@ const MyPosts = (props) => {
 
         </div>
     )
-}
+});
 
 
 export default MyPosts;
